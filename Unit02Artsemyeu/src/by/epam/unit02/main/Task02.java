9aca68a7ea9c224b831604dda583376131135737
andreia <andreia@example.com>
2021-05-11 18:02:33 +0300

    Task02

--- Unit02Artsemyeu/src/by/epam/unit02/main/Task02.java
+++ Unit02Artsemyeu/src/by/epam/unit02/main/Task02.java
@@ -2,6 +2,17 @@ package by.epam.unit02.main;
 
 public class Task02 {
 	public static void main (String[] args) {
+		int x, y, rez;
 		
+		x = 1;
+		y = 100; 
+		
+		if (x < y) {
+			rez = y;
+			y = x; 
+			x = rez;
+		}
+		System.out.println("x: " + x + "\ny: " + y);
 	}
+	
 }
