e1cbd0fae6f8cb0bc2c445df7449c9413bf7412f
A_Artsemyeu <Nika777fm@tut.by>
2021-05-20 17:08:24 +0300

    Task05

--- Unit02Artsemyeu/src/by/epam/unit02/main/Task05.java
+++ Unit02Artsemyeu/src/by/epam/unit02/main/Task05.java
@@ -1,7 +1,14 @@
 package by.epam.unit02.main;
+import static java.lang.Math.*;
 
 public class Task05 {
 	public static void main (String[] args) {
+		double x = 120.01;
+		double y = 140.05;
+				
+		double rezultat = (sin(x) + cos(y) / (cos(x) - sin(y)) * tan(x * y));
+		
+		System.out.println("The value of the expression is exactly - " + rezultat);
 		
 	}
 }
