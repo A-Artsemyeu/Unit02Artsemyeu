4e3305c61b04d61cf7e9ecdae819094948d936d6
A_Artsemyeu <Nika777fm@tut.by>
2021-05-20 18:55:43 +0300

    Task06

--- Unit02Artsemyeu/src/by/epam/unit02/main/Task06.java
+++ Unit02Artsemyeu/src/by/epam/unit02/main/Task06.java
@@ -3,5 +3,17 @@ package by.epam.unit02.main;
 public class Task06 {
 	public static void main (String[] args) {
 		
+		int m = 5135;
+		int n = 1436;
+		
+		double rezult = (double)m / n;
+		System.out.println("Result is " + rezult);
+		
+		int yongerNumber = (int)rezult % 10;
+		int olderNumber = (int)(rezult * 10) % 10;
+		
+		System.out.println("Yonger number is " + yongerNumber );
+		System.out.println("Older number is " + olderNumber );
+		
 	}
 }
